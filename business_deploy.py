b6082e96e54684b3303e7b743975d5e0f3acad86
weibing.pan <pan0721@163.com>
2017-07-11 19:20:28 +0800

    初步提交

---
+++ business_deploy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+import sys,os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print BASE_DIR
+sys.path.append(BASE_DIR)
+from deploy import BusinessDeploy
+
+if __name__ == '__main__':
+    business_deploy = BusinessDeploy.business_deploy(sys.argv)
+

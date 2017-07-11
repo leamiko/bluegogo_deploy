4a79c852cc39a853d0b43b31bda4417c9239c0f6
weibing.pan <pan0721@163.com>
2017-07-11 19:45:45 +0800

    test pycharm commit

---
+++ config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+#_*_ coding:utf-8 _*_
+
+
+deploy_dic = {
+    "interval_num":4,
+    "online_pick_method":"iptables",
+    "gray_pick_method":"nginx",
+    "online":False,
+    "deploy_type_li":["tomcat","jar","static"]
+}
--- deploy.py
+++ deploy.py
@@ -12,7 +12,7 @@ class BusinessDeploy(object):
 
     def argv_parser(self):
         if len(sys.argv) != 3:
-            sys.exit("\033[31;1mWrong number of parameters\033[0m")
+            sys.exit("\033[31;1mWrong number of parameters  [%s]\033[0m")
 
     def fetch_host_list(self):
         pass
